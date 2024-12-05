aad7f1e70c3dfbc386070d838f7563dc9f788229
mrodavell <mail.jomar.llevado@gmail.com>
2024-12-06 04:35:42 +0800

    ABCD Letters

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -26,7 +26,7 @@ export default function DrawerContent(props: any) {
                 throw error;
             }
 
-            router.replace("login");
+            router.replace("/");
 
         } catch (error: any) {
             Alert.alert("Error", error.message, [{ text: 'OK' }]);
