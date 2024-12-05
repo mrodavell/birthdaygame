09932ca58c8885c87cf002095ddcd5261af6fdee
mrodavell <mail.jomar.llevado@gmail.com>
2024-12-06 05:01:09 +0800

    quick fix

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -26,7 +26,7 @@ export default function DrawerContent(props: any) {
                 throw error;
             }
 
-            router.replace("/");
+            router.push("/");
 
         } catch (error: any) {
             Alert.alert("Error", error.message, [{ text: 'OK' }]);
